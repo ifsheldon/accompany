63f34b1dec641fdf4f74725df9a7e92af698db65
ifsheldon <39153080+ifsheldon@users.noreply.github.com>
2021-08-26 00:29:10 +0300

    finish bound!{}

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
+macro_rules! bound {
+    (with $patterned_identifier:pat = $expression:expr=>$bound_code:block)=>{
+       {
+           // patterned_identifier includes examples like tuple destruction, struct destruction and conventional identifier
+           let $patterned_identifier = $expression;
+           {$bound_code}
+       }
+    }
+}
+
+
 #[cfg(test)]
-mod tests {
+mod macro_tests {
+    struct A {
+        pub field: u8,
+        pub field_u16: u16,
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_with_bound() {
+        let a = A {
+            field: 1,
+            field_u16: 2,
+        };
+        let A { field: i, field_u16: j } = a;
+        let simple_result = bound! {
+            with i = 3 => {
+                let m = i+1;
+                m
+            }
+        };
+        let destruct_result = bound! {
+            with A { field: i, field_u16: _j } = a =>{
+                let m = i+1;
+                m
+            }
+        };
+        let detuple_result = bound! {
+            with (i,_j)=(4,3) =>{
+                let m = i+1;
+                m
+            }
+        };
+        println!("{}", simple_result);
+        println!("{}", destruct_result);
+        println!("{}", detuple_result);
     }
 }
